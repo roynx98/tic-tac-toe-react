98b9e6bc1501bad786de0401a14473b6b4a202d8
Roy Rodriguez <roynx98@gmail.com>
2020-11-02 17:29:07 -0600

    test: Board.

--- src/tests/components/Board.test.js
+++ src/tests/components/Board.test.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import { Board } from '../../components/Board';
 
 describe('Board component', () => {
 
-  test('Click an empty cell should display X', () => {
+  test('should match snapshot', async () => {
     const wrapper = shallow(<Board />);
-    // console.log(t.html())
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should display X at first turn', () => {
+    const wrapper = shallow(<Board />);
 
     wrapper.find('.board-row').at(0).childAt(0).simulate('click');
     expect(wrapper.find('.board-row').at(0).childAt(0).prop('value')).toBe('X');
+  });
+  
+  test('should display O at second turn', () => {
+    const wrapper = shallow(<Board />);
+
+    wrapper.find('.board-row').at(0).childAt(0).simulate('click');
+    wrapper.find('.board-row').at(0).childAt(1).simulate('click');
+    expect(wrapper.find('.board-row').at(0).childAt(1).prop('value')).toBe('O');
   });
 
 });
