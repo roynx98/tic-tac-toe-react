a90a453e5be39d87e279db470b60a5f6d3caed9b
Roy Rodriguez <roynx98@gmail.com>
2020-11-03 00:14:06 -0600

    chore: Add docs folder.

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,15 +4,17 @@ import React from 'react';
 export class Game extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+      <>
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+      </>
     );
   }
 }
