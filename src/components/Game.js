0b35bc328cbe53ead98978195eaa39b29860b47a
Roy Rodriguez <roynx98@gmail.com>
2020-11-08 17:29:24 -0600

    test: Game component.

--- src/components/Game.js
+++ src/components/Game.js
@@ -45,19 +45,17 @@ export class Game extends React.Component {
     }
 
     return (
-      <>
-        <div className="game">
-          <div className="game-board">
-            <Board 
-              squares={current.squares}
-              onClick={i => this.handleClick(i)} />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+      <div className="game">
+        <div className="game-board">
+          <Board 
+            squares={current.squares}
+            onClick={i => this.handleClick(i)} />
         </div>
-      </>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
     );
   }
 
--- src/tests/components/Game.test.js
+++ src/tests/components/Game.test.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Game } from '../../components/Game';
+import { calculateWinner } from '../../helpers/calculateWinner';
+import sinon from 'sinon';
+
+jest.mock('../../helpers/calculateWinner');
 
 describe('Game component', () => {
+
+  beforeEach(() => {
+    calculateWinner.mockReturnValue(null);
+  });
 
   test('should match snapshot', () => {
     const wrapper = shallow(<Game />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should display status "Next player: O" as default', () => {
+    const wrapper = shallow(<Game />);
+    expect(wrapper.find('.game-info').childAt(0).text()).toBe('Next player: O');
+  });
+
+  test('should change state to O at first turn', () => {
+    const wrapper = shallow(<Game />);
+    const componentInstance = wrapper.instance();
+    componentInstance.handleClick(0)
+    expect(componentInstance.state.history[1].squares[0]).toBe('O');
+  });
+
+  test('should change state to X at second turn', () => {
+    const wrapper = shallow(<Game />);
+    const componentInstance = wrapper.instance();
+    componentInstance.handleClick(0);
+    componentInstance.handleClick(1);
+    expect(componentInstance.state.history[2].squares[1]).toBe('X');
+  });
+
+  test('should not modify the board after clicking more than once a cell', () => {
+    const setStateSpy = sinon.spy(Game.prototype, 'setState');
+
+    const wrapper = shallow(<Game />);
+    const componentInstance = wrapper.instance();
+
+    componentInstance.handleClick(0);
+    componentInstance.handleClick(0);
+    expect(setStateSpy.calledOnce).toBe(true);
+    setStateSpy.restore();
+  });
+
+  test('should not modify the board after winning', () => {
+    const setStateSpy = sinon.spy(Game.prototype, 'setState');
+    calculateWinner.mockReturnValue('X');
+    const wrapper = shallow(<Game />);
+    const componentInstance = wrapper.instance();
+
+    componentInstance.handleClick(0);
+    componentInstance.handleClick(1);
+    componentInstance.handleClick(2);
+
+    expect(setStateSpy.called).toBe(false);
+    setStateSpy.restore();
   });
 
 });
