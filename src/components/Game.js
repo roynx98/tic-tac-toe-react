9317619991030f6035d1dda179f76ed31b68d9f1
Roy Rodriguez <roynx98@gmail.com>
2020-11-08 21:06:00 -0600

    feat: Handle time travel.

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,33 +11,52 @@ export class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      isXNext: false
+      isXNext: false,
+      stepNumber: 0
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
 
     if (current.squares[i] || calculateWinner(current.squares)) {
       return;
     }
 
-    const newHistory = this.state.history.slice();
     const newSquares = current.squares.slice();
 
     newSquares[i] = this.state.isXNext ? 'X' : 'O';
 
     this.setState({
-      history: newHistory.concat({ squares: newSquares }),
-      isXNext: !this.state.isXNext
+      history: history.concat({ squares: newSquares }),
+      isXNext: !this.state.isXNext,
+      stepNumber: history.length
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      isXNext: (step % 2) === 1,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((_, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status = `Next player: ${this.state.isXNext ? 'X' : 'O'}`;
     if (winner) {
@@ -53,7 +72,7 @@ export class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
